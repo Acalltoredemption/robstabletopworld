7ac0e7cdf0091f91841357366ef493051e6ce062
Acalltoredemption <acalltoredemption@gmail.com>
2021-01-14 15:44:31 -0800

    some code consolidation for the blogposts and upcoming components to return jsx on useeffect

--- src/components/blogposts/blogposts.js
+++ src/components/blogposts/blogposts.js
@@ -11,41 +11,37 @@ const BlogPosts = () => {
      const [blogs, setBlogs] = useState('')
 
 
-function renderBlog(doc){ 
-        let title = doc.data().title;
-        let author = doc.data().author;
-        let content = doc.data().content;
-        let photo = doc.data().photo;
-        setBlogs(
-         <div className="blogpost">
-         <div className="blogbox">
-  <Tilt className="tiltbox">       
-            <div className="blogpost-image">
-            <div className='content'>
-            <h1 className='title'>{title}</h1>
-            </div>
-            <img className="blogimg" src={photo} alt="a blogpost image" /> 
-            </div>
-            </Tilt>
-            </div>
-            <div className="blogpost-author">{author}</div>
-            <div className="blogpost-summary">
-                {content}
-            </div>
-            <div className="buttondiv"> 
-            <button className={Button}>To Article</button>
-         </div>
-        <div>
- </div>
- </div>
-        )
-       
-}
-
  useEffect(() => {
     db.collection('blogposts').get().then((snapshot) => {
         snapshot.docs.forEach(doc => {
-renderBlog(doc);
+
+let title = doc.data().title;
+let author = doc.data().author;
+let content = doc.data().content;
+let photo = doc.data().photo;
+setBlogs( ...blogs,
+ <div className="blogpost">
+ <div className="blogbox">
+<Tilt className="tiltbox">       
+    <div className="blogpost-image">
+    <div className='content'>
+    <h1 className='title'>{title}</h1>
+    </div>
+    <img className="blogimg" src={photo} alt="a blogpost image" /> 
+    </div>
+    </Tilt>
+    </div>
+    <div className="blogpost-author">{author}</div>
+    <div className="blogpost-summary">
+        {content}
+    </div>
+    <div className="buttondiv"> 
+    <button className={Button}>To Article</button>
+ </div>
+<div>
+</div>
+</div>
+)
         })
 })
     }, []);
--- src/components/sidebar/upcoming/upcoming.js
+++ src/components/sidebar/upcoming/upcoming.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import UpcomingImage from '../../../images/blood-bowl-cover.jpg';
 import Tilt from 'react-vanilla-tilt';
 import './upcoming.css';
@@ -7,100 +7,60 @@ import 'firebase/firestore';
 import {db} from '../../../firebase/firebaseconfig';
 
  
-  function renderEvent(doc){
-      
-    let parentdiv = document.createElement('div');
-    let titlep = document.createElement('p');
-    let upcomingbox = document.createElement('div');
-    let imageborder = document.createElement('div');
-    let upcomingcontent = document.createElement('div');
-    let titleh1 = document.createElement('h1');
-    let eventimage = document.createElement('img');
-    let datep = document.createElement('p');
-    let upcomingdesc = document.createElement('div');
-
-    eventimage.src = doc.data().photo;
-    titleh1.textContent = doc.data().title;
-    datep.textContent = doc.data().date;
-    upcomingdesc.textContent = doc.data() .description;
-    
-    titlep.textContent = 'Upcoming Events!';
-
-    parentdiv.className = "upcoming";
-    titlep.className = "upcoming-title";
-    upcomingbox.className = "upcomingbox";
-    imageborder.className = "image-border";
-    upcomingcontent.className = "upcomingcontent";
-    titleh1.className = "upcomingtitle";
-    eventimage.className = "upcomingimg";
-    datep.className = "upcoming-date";
-    upcomingdesc.className = "upcoming-desc";
-
-
-    upcomingcontent.appendChild(titleh1);
-    imageborder.appendChild(upcomingcontent);
-    imageborder.appendChild(eventimage);
-    upcomingbox.appendChild(imageborder);
-    
-    
-  
-
-    parentdiv.appendChild(titlep);
-    parentdiv.appendChild(upcomingbox);
-    parentdiv.appendChild(datep);
-    parentdiv.appendChild(upcomingdesc);
-
-
-
-      document.getElementById('event-list').appendChild(parentdiv);
-  
-  }
-
 
 
 
 
 const Upcoming = () => {
+    const [events, setEvents] = useState('')
 
+    
     useEffect(() => {
         db.collection('events').get().then((snapshot) => {
             snapshot.docs.forEach(doc => {
-                renderEvent(doc);
+               
+                let date = doc.data().date;
+                let description = doc.data().description;
+                let photo = doc.data().photo;
+                let title = doc.data().title;
+        
+                setEvents(
+                    <div className="upcoming">
+                
+                    <p className="upcoming-title">Upcoming Event</p>
+                    <div className="upcomingbox">
+                    <Tilt className="tiltbox"> 
+                <div className="image-border">
+        
+                <div className="upcomingcontent">
+                <h1 className="upcomingtitle">{title}</h1>
+                </div>
+        
+                <img className="upcomingimg" src={photo} alt="image of an upcoming event" />
+                </div>
+                </Tilt>
+                </div>
+        
+        
+                    <p className="upcoming-date">{date}</p>
+                <div className="upcoming-desc">
+                    {description}
+                </div>
+                
+        
+        
+                </div>
+                )
             })
         })
      }, []);
 
 
     return (
-        <div className="upcoming">
-        
-            <p className="upcoming-title">Upcoming Event</p>
-            <div className="upcomingbox">
-            <Tilt className="tiltbox"> 
-        <div className="image-border">
-
-        <div className="upcomingcontent">
-        <h1 className="upcomingtitle">BLOOD BOWL</h1>
-        </div>
-
-        <img className="upcomingimg" src={UpcomingImage} alt="image of an upcoming event" />
-        </div>
-        </Tilt>
-        </div>
-
-
-            <p className="upcoming-date">Event Date</p>
-        <div className="upcoming-desc">
-            Description of the upcoming event!
-        </div>
-        
 
         <ul id="event-list">
-
+{events}
         </ul>
-        </div>
-     
-        
     )
 }
 
