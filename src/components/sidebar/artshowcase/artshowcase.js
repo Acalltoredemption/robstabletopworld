2d9e2b9c1c8cbedce9ddf3d95e117c5551d2ac95
Acalltoredemption <acalltoredemption@gmail.com>
2021-02-03 18:01:25 -0800

    artshowcase component fetching submission data

--- src/components/sidebar/artshowcase/artshowcase.js
+++ src/components/sidebar/artshowcase/artshowcase.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './artshowcase.css';
-import test from '../../../images/test1.jpg';
+import 'firebase/firestore';
+
+import {db} from '../../../firebase/firebaseconfig';
 
 
 const Showcase = () => {
-    return ( 
-        <div>
-        <h4>Community Showcase</h4>
-        <img className="showcase maindiv" src={test} />
-        </div>
-     );
+    const [showcase, setShowcase] = useState('')
+
+    useEffect(() => {
+        db.collection('showcase').orderBy('date', 'asc').get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+               
+                let date = doc.data().date;
+                let photo = doc.data().photo;
+                let name = doc.data().name;
+                console.log(doc.data());
+        
+                setShowcase(
+                    <div>
+                    <h4>Community Showcase</h4>
+                    <img className="showcase maindiv" src={photo} />
+                    <p>{name}</p>
+                    </div>
+                )
+            })
+        })
+     }, []);
+
+
+    return (
+
+        <ul id="showcase-list">
+            {showcase}
+        </ul>
+    )
+
 }
  
 export default Showcase;
