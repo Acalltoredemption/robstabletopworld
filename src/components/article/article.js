5d9c6f5b426bbfaa22aec3c2fdef05b5c7812657
Acalltoredemption <acalltoredemption@gmail.com>
2021-02-16 08:12:25 -0800

    working on article component to isolate specific article

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -1,13 +1,62 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import {db} from '../../firebase/firebaseconfig';
 
 
 const  Article = () => {
 
-return (
+    const [blog, setBlog] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+        
+            await db.collection('blogposts').doc('HGxZ3Bty8CifBKgljk0a').get().then(doc => {
+                this.blogstore = []
+                
+                    var data = {id: doc.id, ...doc.data() };
+                     this.blogstore.push(data)
+                     
+                })
+            ;
+            setBlog(this.blogstore);
+            
+            
+        }
+        
+        fetchPosts();
+    }, []);
+
+
+    return(
+        <div>
+            {
+            blog && 
+            blog.map(blog => {
+                return(
+        <div className="blogpost" key={blog.title}>
+      <div className="blogbox">       
+         <div className="blogpost-image">
+         <div className='content'>
+         <h1 className='title'>{blog.title}</h1>
+         </div>
+         <img className="blogimg"  src={blog.photo} alt="a blogpost" /> 
+         </div>
+         </div>
+         <div className="blogpost-author">{blog.author}</div>
+         <div className="blogpost-summary">
+             {blog.content}
+         </div>
+         <div className="buttondiv"> 
+         <input type="submit" className="btn btn-success btn-send" value="To Article" />
+      </div>
      <div>
-        Hello!
-    </div>
-)
+     </div>
+     </div>
+                            )
+                        })
+                    }
+        </div>
+        
+            )
+
 }
 export default Article;
