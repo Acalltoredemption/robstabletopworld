a5c6612ebe158df8d228c527f40b6cbfd796707e
Acalltoredemption <acalltoredemption@gmail.com>
2021-03-01 18:43:06 -0800

    working on obtaining username from firestore - some error cleanup

--- src/components/sidebar/userlogin/userlogin.js
+++ src/components/sidebar/userlogin/userlogin.js
@@ -1,24 +1,27 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import {useAuth} from '../../../contexts/AuthContext';
 import { useHistory } from 'react-router-dom';
 import './userlogin.css';
 import {Alert} from 'react-bootstrap';
 import Person from '../../../images/person.svg';
 import Lock from '../../../images/lock.svg';
-
+import {db} from '../../../firebase/firebaseconfig';
+import {auth} from '../../../firebase/firebaseconfig';
 
 
 const userLogin = () => {
     const {currentUser, logout} = useAuth();
     const history = useHistory()
     let message = 'Sign In';
-    let adminmessage = '';
     const loggedIn = document.querySelectorAll('#logged-in');
     const loggedOut = document.querySelectorAll('#logged-out');
     const emailRef = useRef();
     const passwordRef = useRef();
     const {login} = useAuth();
     const [error, setError] = useState('');
+    const [username, setUsername] = useState('');
+    var user = auth.currentUser;
+
     
   
     if (currentUser && currentUser.email) {
@@ -26,15 +29,30 @@ const userLogin = () => {
         message = 'Welcome!';
         loggedIn.forEach(item => item.style.display = 'block');
         loggedOut.forEach(item => item.style.display = 'hidden');
-        if (currentUser.uid === 'cw67NhgIsDhyAdp2AMEuFm11a2G2'){
-            adminmessage= 'You are logged in as Admin'
-        }
     } else {
         //toggle UI elements
         message = 'Log in or Sign up';
         loggedIn.forEach(item => item.style.display = 'hidden');
         loggedOut.forEach(item => item.style.display = 'block');
     }
+
+    useEffect(() => {
+        const fetchUsername = async () => {
+        if(currentUser != null){
+            await db.collection('usernames').doc(user).get().then(snapshot => {
+               var userstore = []
+                snapshot.forEach(doc => {
+                    var data = doc.data(); 
+                     userstore.push(data)
+                     setUsername(userstore);
+                })
+            });
+            
+            
+        }
+        }
+        fetchUsername();
+    }, []);
 
 
    async function handleSubmit(e) {
@@ -80,12 +98,12 @@ async function handleLogout() {
         <div className="loginholder loginbox">
         <div className="container">
     <div className="d-flex justify-content-center">
-        <div class="card">
+        <div className="card">
             <div className="card-header cardheader">
-                <p style={{fontWeight: 'bold'}}>{message}</p>
+                <p style={{fontWeight: 'bold'}}>{message} {username.username}</p>
             </div>
         <div className="card-body">
-            <form autocomplete="off" onSubmit={handleSubmit}>
+            <form autoComplete="off" onSubmit={handleSubmit}>
                 <div className="input-group form-group">
                     <div className="input-group-prepend">
                         <span className="input-group-text"><img src={Person} /></span>
@@ -96,7 +114,7 @@ async function handleLogout() {
                     <div className="input-group-prepend">
                         <span className="input-group-text"><img src={Lock} /></span>
                     </div>
-                    <input type="password" ref={passwordRef} class="form-control" placeholder="Password"></input>
+                    <input type="password" ref={passwordRef} className="form-control" placeholder="Password"></input>
                 </div>
                 {error && <Alert variant="danger">{error}</Alert>}
                 <div className="form-group">
