1a314cd6cde67a7c912b7dd3e4f4b60bd3d665a0
Acalltoredemption <acalltoredemption@gmail.com>
2021-03-09 19:51:44 -0800

    setting up about component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import aboutme from '../../images/AboutMe.jpg';
+import './about.css'
 const About = () => {
     return (
         <div>
-
+        <div className="aboutmebox">
+            <div>
+            <img src={aboutme} />
+            </div>
+            <div>
             <p>
             With over 30 years of experience in the hobby, Rob Oren has taught thru his YouTube Channel, "Rob's Tabletop World".   Home of Painting Miniatures 101. Begin adding color to your world as Rob makes painting miniatures simple with easy instructions that anyone can do! Rob has shown a deep and committed passion for both wargames and table top gaming. A family man and a 7 time National Power Lifting Champion, Rob will bring that same commitment and competitive drive for excellence to his video reviews, paint tutorials, and just about everything else he gets into for his wonderful Miniature Market Sponsor as well as his amazing Orenite community family. The man with the biggest heart! Learn why so many people look up to Rob!!
 
@@ -11,6 +16,8 @@ We review new games, have live painting miniatures streams, and have live weeken
 
 Please join us as we have fun and raise money for charities and try to make a difference in everyone's everyday life!
             </p>
+            </div>
+        </div>
         </div>
     )
 }
