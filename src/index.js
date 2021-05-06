e72f678b71f076b40e8e299916a3b906a081f2c4
Acalltoredemption <acalltoredemption@gmail.com>
2021-05-06 11:01:07 -0700

    deploying to heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app.js';
+import App from './app';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "antd/dist/antd.css";
