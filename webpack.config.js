a0d174d8385f5b114a52a2f485f600e68f1bee9f
Acalltoredemption <acalltoredemption@gmail.com>
2021-05-06 11:25:28 -0700

    heroku build attempt

--- webpack.config.js
+++ webpack.config.js
@@ -6,3 +6,13 @@ module.exports = {
         extensions: ['', '.js', '.jsx']
     }
   };
+
+var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+
+var webpackConfig = {
+    plugins: [
+        new CaseSensitivePathsPlugin()
+        // other plugins ...
+    ]
+    // other webpack config ...
+}
